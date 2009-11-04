8419d9c205395c80dacce358d1a985b3f8eea053
Ricardo Signes <rjbs@cpan.org>
2009-11-03 23:14:36 -0500

    move code to Simple.pm if possible

--- lib/Email/Simple.pm
+++ lib/Email/Simple.pm
@@ -1,13 +1,15 @@
+use 5.006;
+use strict;
+use warnings;
 package Email::Simple;
 
-use 5.00503; # why? -- rjbs, 2007-04-01
-use strict;
 use Carp ();
 
+use Email::Simple::Creator;
 use Email::Simple::Header;
 
-$Email::Simple::VERSION = '2.005';
-$Email::Simple::GROUCHY = 0;
+our $VERSION = '2.100';
+our $GROUCHY = 0;
 
 # We are liberal in what we accept.
 sub __crlf_re { qr/\x0a\x0d|\x0d\x0a|\x0a|\x0d/; }
@@ -28,6 +30,21 @@ Email::Simple - simple parsing of RFC2822 message format and headers
   my $old_body = $email->body;
   $email->body_set("Hello world\nSimon");
 
+  print $email->as_string;
+
+...or, to create a message from scratch...
+  
+  my $email = Email::Simple->create(
+      header => [
+        From    => 'casey@geeknest.com',
+        To      => 'drain@example.com',
+        Subject => 'Message in a bottle',
+      ],
+      body => '...',
+  );
+  
+  $email->header_set( 'X-Content-Container' => 'bottle/glass' );
+  
   print $email->as_string;
 
 =head1 DESCRIPTION
@@ -108,6 +125,58 @@ sub _split_head_from_body {
     return (undef, "\n");
   }
 }
+
+=head2 create
+
+  my $email = Email::Simple->create(header => [ @headers ], body => '...');
+
+This method is a constructor that creates an Email::Simple object
+from a set of named parameters. The C<header> parameter's value is a
+list reference containing a set of headers to be created. The C<body>
+parameter's value is a scalar value holding the contents of the message
+body.  Line endings in the body will normalized to CRLF.
+
+If no C<Date> header is specified, one will be provided for you based on the
+C<gmtime> of the local machine. This is because the C<Date> field is a required
+header and is a pain in the neck to create manually for every message. The
+C<From> field is also a required header, but it is I<not> provided for you.
+
+=cut
+
+our $CREATOR = 'Email::Simple::Creator';
+
+sub create {
+  my ($class, %args) = @_;
+
+  # We default it in here as well as below because by having it here, then we
+  # know that if there are no other headers, we'll get the proper CRLF.
+  # Otherwise, we get a message with incorrect CRLF. -- rjbs, 2007-07-13
+  my $headers = $args{header} || [ Date => $CREATOR->_date_header ];
+  my $body    = $args{body} || '';
+
+  my $empty   = q{};
+  my $header  = \$empty;
+
+  for my $idx (map { $_ * 2 } 0 .. @$headers / 2 - 1) {
+    my ($key, $value) = @$headers[ $idx, $idx + 1 ];
+    $CREATOR->_add_to_header($header, $key, $value);
+  }
+
+  $CREATOR->_finalize_header($header);
+
+  my $email = $class->new($header);
+
+  $email->header_set(Date => $CREATOR->_date_header)
+    unless defined $email->header('Date');
+
+  $body = (join $CREATOR->_crlf, split /\x0d\x0a|\x0a\x0d|\x0a|\x0d/, $body)
+        . $CREATOR->_crlf;
+
+  $email->body_set($body);
+
+  return $email;
+}
+
 
 =head2 header_obj
 
--- lib/Email/Simple/Creator.pm
+++ lib/Email/Simple/Creator.pm
@@ -1,8 +1,7 @@
 package Email::Simple::Creator;
 use strict;
 
-use vars qw[$VERSION $CRLF];
-$VERSION = '1.424';
+our $VERSION = '2.100';
 
 sub _crlf {
   "\x0d\x0a";
@@ -24,91 +23,13 @@ sub _finalize_header {
   $$header .= $class->_crlf;
 }
 
-package Email::Simple;
-use strict;
-
-use vars qw[$CREATOR];
-$CREATOR = 'Email::Simple::Creator';
-
-sub create {
-  my ($class, %args) = @_;
-
-  # We default it in here as well as below because by having it here, then we
-  # know that if there are no other headers, we'll get the proper CRLF.
-  # Otherwise, we get a message with incorrect CRLF. -- rjbs, 2007-07-13
-  my $headers = $args{header} || [ Date => $CREATOR->_date_header ];
-  my $body    = $args{body} || '';
-
-  my $empty   = q{};
-  my $header  = \$empty;
-
-  for my $idx (map { $_ * 2 } 0 .. @$headers / 2 - 1) {
-    my ($key, $value) = @$headers[ $idx, $idx + 1 ];
-    $CREATOR->_add_to_header($header, $key, $value);
-  }
-
-  $CREATOR->_finalize_header($header);
-
-  my $email = $class->new($header);
-
-  $email->header_set(Date => $CREATOR->_date_header)
-    unless defined $email->header('Date');
-
-  $body = (join $CREATOR->_crlf, split /\x0d\x0a|\x0a\x0d|\x0a|\x0d/, $body)
-        . $CREATOR->_crlf;
-
-  $email->body_set($body);
-
-  return $email;
-}
-
 1;
 
 __END__
 
 =head1 NAME
 
-Email::Simple::Creator - build an Email::Simple object from scratch
-
-=head1 SYNOPSIS
-
-  use Email::Simple;
-  use Email::Simple::Creator;
-  
-  my $email = Email::Simple->create(
-      header => [
-        From    => 'casey@geeknest.com',
-        To      => 'drain@example.com',
-        Subject => 'Message in a bottle',
-      ],
-      body => '...',
-  );
-  
-  $email->header_set( 'X-Content-Container' => 'bottle/glass' );
-  
-  print $email->as_string;
-
-=head1 DESCRIPTION
-
-This software provides a constructor to L<Email::Simple|Email::Simple> for
-creating messages from scratch.
-
-=head1 METHODS
-
-=head2 create
-
-  my $email = Email::Simple->create(header => [ @headers ], body => '...');
-
-This method is a constructor that creates an Email::Simple object
-from a set of named parameters. The C<header> parameter's value is a
-list reference containing a set of headers to be created. The C<body>
-parameter's value is a scalar value holding the contents of the message
-body.  Line endings in the body will normalized to CRLF.
-
-If no C<Date> header is specified, one will be provided for you based on the
-C<gmtime> of the local machine. This is because the C<Date> field is a required
-header and is a pain in the neck to create manually for every message. The
-C<From> field is also a required header, but it is I<not> provided for you.
+Email::Simple::Creator - private helper for building Email::Simple objects
 
 =head1 PERL EMAIL PROJECT
 
